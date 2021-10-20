c7ce76fa2e6f88ec656e63581c5b6911bd8e12fc
Poós Dániel <poosdani@gmail.com>
2021-10-20 23:13:52 +0200

    kinyert fun done

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView Gep_kep, Felh_kep;
     private TextView Win, Lose;
     private int wincount, losecount, drawcount;
+    private int felhtipp, geptipp;
     private Toast toast;
     private AlertDialog dialog;
 
@@ -24,24 +25,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         Ko.setOnClickListener(new View.OnClickListener() {
-                                  public void onClick(View v) {
-                                      Felh_kep.setImageResource(R.drawable.rock);
-                                  }
+            public void onClick(View v) {
+                Felh_kep.setImageResource(R.drawable.rock);
+                felhtipp = 0;sorsol();
+                kiNyert();
+            }
 
-                              });
+        });
         Papir.setOnClickListener(new View.OnClickListener() {
-                                   public void onClick(View v) {
-                                       Felh_kep.setImageResource(R.drawable.paper);
-                                             }
+            public void onClick(View v) {
+                Felh_kep.setImageResource(R.drawable.paper);
+                felhtipp = 1;sorsol();
+                kiNyert();
+            }
 
-                                         });
+        });
         Ollo.setOnClickListener(new View.OnClickListener() {
-                                   public void onClick(View v) {
-                                       Felh_kep.setImageResource(R.drawable.scissors);
-                                                    }
+            public void onClick(View v) {
+                Felh_kep.setImageResource(R.drawable.scissors);
+                felhtipp = 2;sorsol();
+                kiNyert();
+            }
 
-                                                });
-                                kiNyert();
+        });
+
+
     }
     public void init(){
         Ko = findViewById(R.id.ko);
@@ -51,9 +59,50 @@ public class MainActivity extends AppCompatActivity {
         Felh_kep = findViewById(R.id.felh_kep);
         Win = findViewById(R.id.win);
         Lose = findViewById(R.id.lose);
+        wincount = 0;
+        losecount = 0;
+        drawcount = 0;
+        toast = new Toast(getApplicationContext());
+
     }
 
+    public void createToast() {
+        toast.setText("");
+    }
+
+    public void sorsol(){
+        geptipp = (int)(Math.random()*2);
+        switch (geptipp){
+            case 0: Gep_kep.setImageResource(R.drawable.rock); break;
+            case 1: Gep_kep.setImageResource(R.drawable.paper);break;
+            case 2: Gep_kep.setImageResource(R.drawable.scissors);break;
+            default:break;
+        }
+    }
     public void kiNyert(){
+        if (geptipp == 0 && felhtipp == 1){
+            wincount++;
+            Win.setText(wincount);
+        }else if (geptipp == 1 && felhtipp == 2){
+            wincount++;
+            Win.setText(wincount);
+
+        }else if (geptipp == 2 && felhtipp == 0){
+            wincount++;            Win.setText(wincount);
+
+
+        }else if (felhtipp == 0 && geptipp == 1){
+            losecount++;            Lose.setText(losecount);
+
+        }else if (felhtipp == 1 && geptipp == 2){
+            losecount++;            Lose.setText(losecount);
+
+        }else if (felhtipp == 2 && geptipp == 0){
+            losecount++;            Lose.setText(losecount);
+
+        }else{
+            drawcount++;
+        }
 
     }
 }
